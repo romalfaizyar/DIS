782b611cbeae101d29b50da0cc1c724aa11da601
Romal Faizyar <romal.faizyar@gmail.com>
2021-11-27 15:32:10 +0430

    web

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/hooo', function () {
+    return view('welcome');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
